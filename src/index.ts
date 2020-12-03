47cc6f4b875ab14c874b0cc064b7dce4b00c8069
tian <istiancz@gmail.com>
2020-12-03 22:33:24 +0800

    on off emit

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,18 @@ class EventHub {
         this.cache[eventName] = this.cache[eventName] || [];
         (this.cache[eventName]).push(fn)
     }
-    emit(eventName) {
-        (this.cache[eventName] || []).forEach(fn => fn())
+    emit(eventName, content?) {
+        (this.cache[eventName] || []).map(fn => fn(content))
+    }
+    off(eventName, fn) {
+        this.cache[eventName] = this.cache[eventName] || [];
+        let index = this.cache[eventName].indexOf(fn)
+        if (index > -1) {
+            this.cache[eventName].splice(index, 1)
+        } else {
+            return
+        }
+
     }
 }
 export default EventHub
--- test/index.ts
+++ test/index.ts
@@ -2,9 +2,18 @@ import EventHub from '../src/index'
 
 const eventHub = new EventHub
 
-eventHub.on('click', () => {
-    console.log('clicked!');
+eventHub.on('click', (content) => {
+    console.log(content);
 })
 
-eventHub.emit('click')
+eventHub.emit('click', 'i clicked!')
 
+const eventHub2 = new EventHub
+let called = false
+const fn1 = () => { called = true; }
+eventHub.on('sub', fn1)
+eventHub.off('sub', fn1)
+eventHub.emit('sub')
+setTimeout(() => {
+    console.log(called);
+}, 1000);
